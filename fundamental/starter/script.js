4919e88274cfab9b3901f343223837b6bfe663f5
Payal Sasmal <sasmalpayal@gmail.com>
2022-04-02 23:16:16 +0530

    basic fundamental

---
+++ fundamental/starter/script.js
@@ -0,0 +1,11 @@
+let js ="amazing"
+if (js === "amazing") alert("Beware of Payal");
+
+console.log(40+8);
+console.log(40+9);
+
+let firstName = "Payal"
+let PI = 3.145
+
+console.log(firstName)
+console.log(PI)
