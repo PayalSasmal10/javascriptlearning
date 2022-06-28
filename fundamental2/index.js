e624cccbb8ba315be882fddfb558cec512f2425a
Payal Sasmal <sasmalpayal@gmail.com>
2022-06-28 21:12:25 +0530

    learning function

--- fundamental2/index.js
+++ fundamental2/index.js
@@ -3,3 +3,14 @@ function logger(){
 }
 
 logger();
+
+// parameterized function 
+
+function fruitProceesor(apples, grapes){
+    console.log(apples,grapes);
+    const juice = `Juice with ${apples} apples and ${grapes} grapes`;
+    return juice;
+}
+
+const finalJuice = fruitProceesor(2,3);
+console.log(finalJuice);
