8bfed196e64748d2bc7a2d4bf7d6637a5e08697c
Payal Sasmal <sasmalpayal@gmail.com>
2022-06-30 16:18:05 +0530

    challenge completed

--- fundamental2/challenge/index.js
+++ fundamental2/challenge/index.js
@@ -21,3 +21,33 @@ function checkWinner(){
 }
 
 checkWinner();
+
+
+// 2nd way to do it.
+console.log("##########################################################");
+
+const calcAverage1 = (fs, ss, ts)=> (fs+ss+ts) /3 ;
+console.log(calcAverage1(12, 13, 45));
+
+// TEST1
+const scoreKoalas = calcAverage1(44,23,71);
+const scoreDolhins = calcAverage1(65, 54, 49);
+console.log(scoreKoalas, scoreDolhins);
+
+
+
+const checkWinner1 = function (avgDolhins1, avgKoalas1){
+    if (avgDolhins1 >= 2* avgKoalas1){
+        console.log(`Dolhins win (${avgDolhins1} vs. ${avgKoalas1})`);
+    }else if(avgDolhins1 <= 2* avgKoalas1){
+        console.log(`Koalas win (${avgKoalas1} vs. ${avgDolhins1})`);
+    }
+    else{
+        console.log(`it's a draw`);
+    }
+}
+
+checkWinner1(scoreDolhins, scoreKoalas);
+checkWinner1(788,122);
+
+// TEST2
