e3371e5c1a27c3d502ba568eb8e8b141c57a3267
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-03 12:12:51 +0530

    learning array in js

---
+++ fundamental2/array/index.js
@@ -0,0 +1,12 @@
+// array methods
+
+// add elements at the end of the array 
+const numberArray = [4, 5, 9.4, 'payal', 1];
+numberArray.push(10);
+console.log(numberArray);
+
+
+// add element at first position of the element in array.
+numberArray.unshift('sasmal');
+console.log(numberArray);
+
