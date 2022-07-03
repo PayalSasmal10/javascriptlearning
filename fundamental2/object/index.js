aeecbf15f09cbeac9a341c24fb01b79cd53ce674
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-03 19:42:20 +0530

    learning object in javascript

---
+++ fundamental2/object/index.js
@@ -0,0 +1,14 @@
+// object 
+
+const person = {
+    firstName: 'Payal',
+    lastName: 'Sasmal',
+    age: 2022-1994,
+    job: 'Software Developer',
+    friends: ['A', 'B', 'C'],
+};
+
+console.log(person);
+console.log(person.firstName);
+console.log(person['job']);
+
