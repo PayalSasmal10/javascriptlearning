1f0427ba46de0e906608479e76bdebcf2046752d
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-03 20:18:19 +0530

    learning object in javascript

--- fundamental2/object/index.js
+++ fundamental2/object/index.js
@@ -12,3 +12,22 @@ console.log(person);
 console.log(person.firstName);
 console.log(person['job']);
 
+// bracket notation usages.
+// const interestedIn = prompt("Choose these values to get info of payal, firstName, lastName, age, job and friednds ");
+
+// if(person[interestedIn]){
+//     console.log(person[interestedIn]);
+// }else{
+//     console.log("Wrong request!. please choose correct one");
+
+// }
+
+person.location = 'India';
+person['twitter'] = 'payalsasmal';
+console.log(person);
+
+console.log(person['firstName']+' has ' + person['friends'].length+ ' friends, and his best friend is called ' + person.friends[0]);
+console.log(`${person.firstName} has ${person.friends.length} friends, and his best friend is called ${person.friends[0]}`);
+
+
+
