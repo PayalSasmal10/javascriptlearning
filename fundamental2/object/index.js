9607b457868229dc03913165c6dd0884bf4e3c46
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-05 20:51:17 +0530

    cal age normal way

--- fundamental2/object/index.js
+++ fundamental2/object/index.js
@@ -1,33 +1,46 @@
 // object 
 
+// const person = {
+//     firstName: 'Payal',
+//     lastName: 'Sasmal',
+//     age: 2022-1994,
+//     job: 'Software Developer',
+//     friends: ['A', 'B', 'C'],
+// };
+
+// console.log(person);
+// console.log(person.firstName);
+// console.log(person['job']);
+
+// // bracket notation usages.
+// const interestedIn = prompt("Choose these values to get info of payal, firstName, lastName, age, job and friednds ");
+
+// if(person[interestedIn]){
+//     console.log(person[interestedIn]);
+// }else{
+//     console.log("Wrong request!. please choose correct one");
+
+// }
+
+// person.location = 'India';
+// person['twitter'] = 'payalsasmal';
+// console.log(person);
+
+// console.log(person['firstName']+' has ' + person['friends'].length+ ' friends, and his best friend is called ' + person.friends[0]);
+// console.log(`${person.firstName} has ${person.friends.length} friends, and his best friend is called ${person.friends[0]}`);
+
+
+
 const person = {
     firstName: 'Payal',
     lastName: 'Sasmal',
     age: 2022-1994,
     job: 'Software Developer',
     friends: ['A', 'B', 'C'],
+    hasDRiverLicense: true,
+    calcAge: function(birthyear) {
+        return 2022 - birthyear;
+    }
 };
 
-console.log(person);
-console.log(person.firstName);
-console.log(person['job']);
-
-// bracket notation usages.
-const interestedIn = prompt("Choose these values to get info of payal, firstName, lastName, age, job and friednds ");
-
-if(person[interestedIn]){
-    console.log(person[interestedIn]);
-}else{
-    console.log("Wrong request!. please choose correct one");
-
-}
-
-person.location = 'India';
-person['twitter'] = 'payalsasmal';
-console.log(person);
-
-console.log(person['firstName']+' has ' + person['friends'].length+ ' friends, and his best friend is called ' + person.friends[0]);
-console.log(`${person.firstName} has ${person.friends.length} friends, and his best friend is called ${person.friends[0]}`);
-
-
-
+console.log(person.calcAge(1994));
