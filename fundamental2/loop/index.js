560600cb435021c244f2f976832b8f989fbf4f87
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-07 16:20:37 +0530

    loop with array

--- fundamental2/loop/index.js
+++ fundamental2/loop/index.js
@@ -19,7 +19,9 @@ const years = [1990, 1991, 1992, 1993, 1994, 1995, 1996];
 const age = [];
 
 for (let i= 0; i < years.length; i++){
-    age [i] = 2022- years[i];
+    // age[i] = 2022- years[i];
+    age.push(2022 - years[i]);
 }
 
 console.log(age);
+
