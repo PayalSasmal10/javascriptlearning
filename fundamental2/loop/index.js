d8e275ad3a3570dedbb7a613a08c2c7297b5af1d
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-07 16:18:00 +0530

    loop with array

---
+++ fundamental2/loop/index.js
@@ -0,0 +1,25 @@
+const type = [];
+const type2 = [];
+for (let i =0; i< 5; i++){
+    // console.log(i);
+    //  assigning values in array one way
+    type [i] = typeof i;
+
+    // another way to assigning value in array
+    type2.push(typeof i);
+
+}
+
+console.log(type);
+console.log(type2);
+
+//  calculate age using array
+console.log(".................................................")
+const years = [1990, 1991, 1992, 1993, 1994, 1995, 1996];
+const age = [];
+
+for (let i= 0; i < years.length; i++){
+    age [i] = 2022- years[i];
+}
+
+console.log(age);
