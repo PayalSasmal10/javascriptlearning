52c0b7dc23d527358a07aa772c1ceabc1fa9ead3
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-08 20:27:10 +0530

    guess the number

--- dom/guess my number/script.js
+++ dom/guess my number/script.js
@@ -9,12 +9,50 @@ document.querySelector('.score').textContent = 21;
 document.querySelector('.guess').value = 80;
 console.log(document.querySelector('.guess').value);*/
 
+let score = 20;
+
+const secretNumber = Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
     if(!guess){
         document.querySelector('.message').textContent = '⛔ No Number';
     }
+
+    else if( guess === secretNumber){
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+    }
+    else if(guess > secretNumber){
+        if (score > 1){
+            document.querySelector('.message').textContent = '📈 Too High';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            document.querySelector('.message').textContent = '💥 You Lost the Game!';
+            document.querySelector('.score').textContent = 0;
+        }
+        
+    }
+    else if(guess < secretNumber){
+        if (score > 1){
+            document.querySelector('.message').textContent = '📉 Too Low';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            document.querySelector('.message').textContent = '💥 You Lost the Game!';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+})
+
+document.querySelector('.again').addEventListener('click', function () {
+    
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').textContent = '?';
 })
 
 
