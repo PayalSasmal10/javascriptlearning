76ba385f2abf8a8c21ae9bcae48c0ce2892eca22
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-08 21:40:54 +0530

    guess the number updated

--- dom/guess my number/script.js
+++ dom/guess my number/script.js
@@ -10,6 +10,7 @@ document.querySelector('.guess').value = 80;
 console.log(document.querySelector('.guess').value);*/
 
 let score = 20;
+let highscore = 0;
 
 const secretNumber = Math.trunc(Math.random()*20)+1;
 document.querySelector('.number').textContent = secretNumber;
@@ -27,6 +28,11 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('body').style.backgroundColor = '#60b347';
 
         document.querySelector('.number').style.width = '30rem';
+
+        if (score > highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
     }
     else if(guess > secretNumber){
         if (score > 1){
@@ -54,9 +60,15 @@ document.querySelector('.check').addEventListener('click', function () {
 })
 
 document.querySelector('.again').addEventListener('click', function () {
-    
+    score = 20;
     document.querySelector('.guess').value = '';
-    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    
+    // style manipulation
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 })
 
 
