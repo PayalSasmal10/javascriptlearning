ad43d6a23458a092fbc1535124322d4c88514c47
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-09 21:33:11 +0530

    modal number

--- dom/modal/script.js
+++ dom/modal/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModel = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+// console.log(btnsShowModal);
+
+for(let i = 0; i < btnsShowModal.length; i++){
+    console.log(btnsShowModal[i].textContent);
+}
+    
