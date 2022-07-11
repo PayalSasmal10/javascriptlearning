54eacc7c3fa06889b308549b9219274adeda788d
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-11 16:51:47 +0530

    open modal and close modal is done

--- dom/modal/script.js
+++ dom/modal/script.js
@@ -2,11 +2,22 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const closeModel = document.querySelector('.close-modal');
+const btncloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 // console.log(btnsShowModal);
 
 for(let i = 0; i < btnsShowModal.length; i++){
-    console.log(btnsShowModal[i].textContent);
+    btnsShowModal[i].addEventListener("click", function(){
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
 }
-    
+
+const closeModal = function(){
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+btncloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
