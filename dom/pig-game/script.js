448028f6bbeede59ea5fcfa97b691b9dbdce3d0a
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-12 17:22:42 +0530

    resetting all

--- dom/pig-game/script.js
+++ dom/pig-game/script.js
@@ -12,55 +12,96 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-console.log(btnHold);
 
-let currentScore = 0;
-let activePlayer = 0;
-let score = [0,0];
-let total = 0;
-// setting score elements
-score0El.textContent = 0;
-score1El.textContent = 0;
+let currentScore, activePlayer, score, play;
 
-// hide dice
-diceEl.classList.add('hidden');
+
+const initial = function(){
+
+    currentScore = 0;
+    activePlayer = 0;
+    score = [0,0];
+    play = true;
+
+    // setting score elements
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+
+    // hide dice
+    diceEl.classList.add('hidden');
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+}
+
+initial();
+
+const switchNextPlay = function(){
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-    // 1. Generate random dice roll.
-    const randomDice = Math.trunc(Math.random()*6)+1;
+    if (play){
 
-    // 2. Display dice roll.
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${randomDice}.png`;
+    
+        // 1. Generate random dice roll.
+        const randomDice = Math.trunc(Math.random()*6)+1;
 
-    // dice r0ll number is 1 or not.
-    if (randomDice !== 1){
-        // add dice to current score
-        currentScore += randomDice;
+        // 2. Display dice roll.
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${randomDice}.png`;
 
-        // add current score to players.
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-        
-        // current0El.textContent = currentScore;
-    }else
-    {
-        // Switch to next player
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
-        
+        // dice r0ll number is 1 or not.
+        if (randomDice !== 1){
+            // add dice to current score
+            currentScore += randomDice;
+
+            // add current score to players.
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+            
+            // current0El.textContent = currentScore;
+        }else
+        {
+            // Switch to next player
+            
+            switchNextPlay();
+        }
     }
 })
 
 // holding scoire feature
 const holdScore = function() {
-    total += currentScore;
-    console.log(`inside hold score ${total}`);
-    document.getElementById(`score--${activePlayer}`).textContent = total;
+    // 1.Add current score to total
+    
+    score[activePlayer] += currentScore;
+    
+    document.getElementById(`score--${activePlayer}`).textContent = score[activePlayer];
+    
+    // 2. check if score is > 100
+    if (score[activePlayer] >= 20){
+        play = false;
+        diceEl.classList.add('hidden');
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    }
+
+    // switch to next player.
+    switchNextPlay();
 } 
 
 btnHold.addEventListener('click', holdScore);
+
+btnNew.addEventListener('click', initial);
     
