581ab021108883e4f1ee161653a9798523658b57
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-12 16:12:19 +0530

    holding score feature is completed

--- dom/pig-game/script.js
+++ dom/pig-game/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // selecting element
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -10,7 +12,12 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+console.log(btnHold);
+
 let currentScore = 0;
+let activePlayer = 0;
+let score = [0,0];
+let total = 0;
 // setting score elements
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -31,10 +38,28 @@ btnRoll.addEventListener('click', function () {
     if (randomDice !== 1){
         // add dice to current score
         currentScore += randomDice;
+
         // add current score to players.
-        current0El.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        
+        // current0El.textContent = currentScore;
     }else
     {
         // Switch to next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+        
     }
 })
+
+const holdScore = function() {
+    total += currentScore;
+    console.log(`inside hold score ${total}`);
+    document.getElementById(`score--${activePlayer}`).textContent = total;
+} 
+
+btnHold.addEventListener('click', holdScore);
+    
