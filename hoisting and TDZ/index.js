0bbbe56ed2bb6cc812fb9dc06d419dd318144826
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-15 15:46:02 +0530

    hoisting and TDZ is in progress

--- hoisting and TDZ/index.js
+++ hoisting and TDZ/index.js
@@ -12,19 +12,32 @@ const year = 1994;
 
 
 // console.log(addDecl(2, 3));
-// TDZ is happening here
+// TDZ is happening here as i have used const, now will change to var. after adding var we are getting Error:addExp is not a function
 // console.log(addExp(2, 3));
-console.log(addArrow(2, 3));
+// TDZ is happening here too. As i have used const, now will change to var.after adding var we are getting Error:addExp is not a function
+// console.log(addArrow(2, 3));
 
 //function declaration 
 function addDecl(a, b){
     return a + b;
 }
 
-// function expression
-// const addExp = function(a, b) {
-//     return a + b;
-// }
+// function expression -
+var addExp = function(a, b) {
+    return a + b;
+}
 
 // arrow function
-const addArrow = (a, b) => a + b;
+var addArrow = (a, b) => a + b;
+
+
+// Example of hoisting
+
+console.log(numProducts);
+if(!numProducts) deleteShoppingCart();
+
+const numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log("deleted all product");
+}
