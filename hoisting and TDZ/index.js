ed50ac8dd24ce9dd3011a75dddbaddcbb6b96517
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-15 16:01:21 +0530

    hoisting and TDZ is in progress

--- hoisting and TDZ/index.js
+++ hoisting and TDZ/index.js
@@ -36,8 +36,26 @@ var addArrow = (a, b) => a + b;
 console.log(numProducts);
 if(!numProducts) deleteShoppingCart();
 
-const numProducts = 10;
+var numProducts = 10;
 
 function deleteShoppingCart() {
     console.log("deleted all product");
 }
+
+// Example
+var x = 1 ; // This section can add variable on window object in javascript but let and const don't do that.
+let y = 2;
+const z = 3;
+
+console.log( x === window.x);
+console.log( y === window.y);
+console.log( z === window.z);
+
+
+
+/*
+Note:
+1. If we use var then we can get undefined.
+2. If we use const and let then we get ReferenceError: variable can't access after initializing.
+3. Aolways try to use const. let only can be used if we are changing the value later . don't use var for declaring the variable.
+*/
