5bd4ff3ac8fc5ece059f16c9079dd2130affd41a
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-15 18:39:12 +0530

    this keyword learning with regular function vs arrow function

--- thisKeyword/script.js
+++ thisKeyword/script.js
@@ -42,7 +42,7 @@ prince.calAge();
 // f function example to show how to get error as f is just a function and does not contain the value of year so getting undifined can't have year.
 
 const f = payal.calAge;
-f();
+// f();
 
 /*
 Note-
@@ -50,4 +50,33 @@ Note-
 2. Inside function(function expression example), it will be undifined if we use as strict mode else it will be the global window object.
 3. Inside function(arrow function), it will be pointing to window object for arrow function.
 4. for object example, we are checking calcAge
+*/
+
+console.log("...........................Regular vs Arrow Function....................")
+// Regular function vs Arrow function lecture
+
+var firstName = "Payal";
+const oli = {
+    firstName : "Oli",
+    year : 1994,
+    calcAge : function() {
+        console.log(this);
+        console.log(2022-this.year);
+        console.log(`Hey ${this.firstName} from calcAge function`);
+    },
+
+    greet: () => {
+        console.log(this);
+        console.log(`Hey ${this.firstName} from global scope`);
+    },
+}
+oli.calcAge();
+oli.greet();
+
+
+
+
+/*
+Note
+1. Arrow function should not give local scope of object so output is undefinied.
 */
