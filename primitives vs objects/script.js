c00c5500475bf4d4911940b34005746b8b99153d
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-16 17:09:37 +0530

    primitives andd reference learning

--- primitives vs objects/script.js
+++ primitives vs objects/script.js
@@ -17,3 +17,18 @@ const marriedPayal = payal;
 marriedPayal.lastName = 'Payal Kumar Prince';
 console.log('Before marriage', payal);
 console.log('After marriage', marriedPayal);
+
+//  how to copy an object to another variable
+
+// marriedPayal = {};
+
+const payal2 = {
+    firstName : 'Payal',
+    lastName : 'Sasmal',
+    age : 27,
+}
+
+const payalCopy = payal;
+payalCopy.lastName = 'Payal Kumar Prince';
+console.log('Before marriage', payal);
+console.log('After marriage', marriedPayal);
