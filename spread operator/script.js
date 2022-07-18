5d2c87bae2d75b57a932885027e8b7d9f92c1cc5
Payal Sasmal <sasmalpayal@gmail.com>
2022-07-18 16:22:11 +0530

    learning spread operator

--- spread operator/script.js
+++ spread operator/script.js
@@ -10,3 +10,56 @@ const newArr = [8, 10, ...arr];
 console.log(newArr);
 
 console.log(...newArr);
+
+const restaurant = {
+    name : 'Classical Italiano',
+    location1 : 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories : ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu : ['Pizza', 'Pasta', 'Risotto'],
+    openingHours : {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0,
+            close: 24,
+        },
+    },
+    order : function(startIndex, mainIndex){
+        return [this.starterMenu[startIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function({starterIndex = 1, address, mainIndex = 0, time = '20:00'}) {
+        // console.log(obj);
+        console.log(`Order Delivered! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    }
+};
+
+
+// Merge array from restaurant object
+
+const newMenu = [...restaurant.mainMenu, 'Butter Paneer masala'];
+console.log(newMenu);
+
+
+// shallow Copy to an array 
+
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log(mainMenuCopy);
+
+// Join 2 arrays
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+
+/*
+Note:
+1. Spread opeartor is a diff from destructring- it takes the elements from the array.
+and it doesn't create new variables.AS a consequence, we only can use in places where we would otherwise
+we write values sperate by commas
+*/
